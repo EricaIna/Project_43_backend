9397d184a205cca3daa7dab796ae43fb2457f5f5
tahha00 <142687319+tahha00@users.noreply.github.com>
2024-01-09 15:28:17 +0000

    added review routes

--- application/reviews/routes.py
+++ application/reviews/routes.py
@@ -10,6 +10,7 @@ from .controllers import (
 
 app = Flask(__name__)
 
+# Create review
 @app.route('/reviews', methods=['POST'])
 @login_required
 def create_review_route():
@@ -22,13 +23,29 @@ def create_review_route():
     result = create_review(movie_id, title, content, rating, user_id)
     return jsonify(result)
 
+# Get reviews
 @app.route('/reviews/<int:movie_id>', methods=['GET'])
 def get_reviews_route(movie_id):
     result = get_reviews(movie_id)
     return jsonify(result)
 
+# Update reviews
 @app.route('/reviews/<int:review_id>', methods=['PATCH'])
 @login_required
 def update_review_route(review_id):
     new_data = request.json
+    user_id = current_user.id
+    result = update_review(review_id, user_id, new_data)
+    return jsonify(result)
+
+# Delete review
+@app.route('/reviews/<int:review_id>', methods=['DELETE'])
+@login_required
+def delete_review_route(review_id):
+    user_id = current_user.id
+    result = delete_review(review_id, user_id)
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run()
     
