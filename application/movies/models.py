758e8e271fb3bac43fbc3bdf578f089d817ead51
gajani-yoga <jeyagajani@hotmail.co.uk>
2024-01-10 16:39:58 +0000

    modify movies endpoint

--- application/movies/models.py
+++ application/movies/models.py
@@ -3,18 +3,11 @@ from .. import db
 class Movie(db.Model):
     __tablename__ = "movies"
     id = db.Column(db.Integer, primary_key=True)
-    # level_0 = db.Column(db.Integer, nullable=False)
-    # index = db.Column(db.Integer, nullable=False)
-    # budget = db.Column(db.Integer, nullable=False)
     genres = db.Column(db.String(200), nullable=False)
-    # keywords = db.Column(db.String(500), nullable=False)
     original_language = db.Column(db.String(3), nullable=False)
     original_title = db.Column(db.String(300), nullable=False)
     overview = db.Column(db.String(1000), nullable=False)
     vote_average = db.Column(db.Float, nullable=False)
-    # name = db.Column(db.String(100), nullable=False)
-    # population = db.Column(db.Integer, nullable=False)
-    # capital_city = db.Column(db.String(100), nullable=False)
     release_date = db.Column(db.Date, nullable=False)
     director = db.Column(db.String(100), nullable=False)
 
@@ -37,9 +30,10 @@ class Movie(db.Model):
         return {
             "id": self.id,
             "original_title": self.original_title,
-            # "original_language": self.original_language,
-            # "overview": self.overview,
-            # "vote_average": self.vote_average,
-            # "release_date": self.release_date,
-            # "director": self.director
+            "original_language": self.original_language,
+            "overview": self.overview,
+            "genres": self.genres,
+            "vote_average": self.vote_average,
+            "release_date": self.release_date,
+            "director": self.director
         }
