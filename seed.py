c7b3638bde40678c8f9709391a1c8a3bf544c9fb
gajani-yoga <jeyagajani@hotmail.co.uk>
2024-01-11 14:45:11 +0000

    add genre endpoint

--- seed.py
+++ seed.py
@@ -13,8 +13,8 @@ print("Dropping Database")
 
 result = db.session.execute(text('DROP TABLE IF EXISTS movies;'))
 db.session.commit()
-
 print("saving movies")
+
 df = pd.read_csv("./movie_dataset.csv")
 df.to_sql(name='movies', con=db.engine)
 print("movies dataset saved")
---
+++ tests/test_movies.py
@@ -0,0 +1,5 @@
+import requests
+
+ENDPOINT = "http://localhost:4000/movies"
+
+response = requests.get(ENDPOINT)
